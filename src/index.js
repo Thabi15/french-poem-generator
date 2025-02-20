b5a70abb2a0970b7d1e0ab12bf487045e5a65dd9
Thabi15 <janemadute@gmail.com>
2025-02-20 14:20:23 +0200

    fixed form style and user experience

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ function generatePoem(event) {
 
   let instructionInput = document.querySelector("#user-instructions");
   let apiKey = "68b0ffbbbf6d90tda73d3f86ae54316o";
-  let context = `You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br/ >. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' with a <strong> element at the end of the poem and NOT at the beginning. `;
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br/ >. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' with a <strong> element at the end of the poem and NOT at the beginning.";
   let prompt = `User instructions: Generate a French poem about ${instructionInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
